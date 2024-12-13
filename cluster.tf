ca92d112f44c87443b773fcfe44ca1ccda116cff
Francisco Ferrinho <ferrinho@gmail.com>
2024-12-13 01:00:31 +0000

    update

---
+++ cluster.tf
@@ -0,0 +1,194 @@
+data "google_project" "main" {
+  project_id = var.project_id
+}
+
+resource "google_container_cluster" "main" {
+  name                = var.cluster_name
+  location            = var.node_locations != null ? null : var.cluster_location
+  node_locations      = var.node_locations
+  deletion_protection = var.enable_cluster_deletion_protection
+  allow_net_admin     = var.enable_autopilot == true ? true : var.allow_net_admin
+  description         = var.cluster_description
+  enable_autopilot    = var.enable_autopilot
+  initial_node_count  = var.initial_node_count
+  networking_mode     = var.networking_mode
+  network             = var.network
+
+  addons_config {
+    horizontal_pod_autoscaling {
+      disabled = var.addons_config.disable_horizontal_pod_autoscaling
+    }
+    http_load_balancing {
+      disabled = var.addons_config.disable_http_load_balancing
+    }
+    network_policy_config {
+      disabled = var.addons_config.disable_network_policy_config
+    }
+    gcp_filestore_csi_driver_config {
+      enabled = var.addons_config.enable_gcp_filestore_csi_driver
+    }
+    gcs_fuse_csi_driver_config {
+      enabled = var.addons_config.enable_gcs_fuse_csi_driver
+    }
+
+    dynamic "cloudrun_config" {
+      for_each = var.addons_config.cloudrun_config != null ? [var.addons_config.cloudrun_config] : []
+      content {
+        disabled           = cloudrun_config.value.disable_cloudrun_config
+        load_balancer_type = cloudrun_config.value.load_balancer_type
+      }
+    }
+  }
+
+  dynamic "cluster_autoscaling" {
+    for_each = var.cluster_autoscaling.enable_cluster_autoscaling ? [var.cluster_autoscaling] : []
+    content {
+      enabled = cluster_autoscaling.value.enable_cluster_autoscaling
+
+      dynamic "resource_limits" {
+        for_each = cluster_autoscaling.value.resource_limits != null ? cluster_autoscaling.value.resource_limits : []
+        content {
+          resource_type = resource_limits.value.resource_type
+          minimum       = resource_limits.value.minimum
+          maximum       = resource_limits.value.maximum
+        }
+      }
+
+      dynamic "auto_provisioning_defaults" {
+        for_each = cluster_autoscaling.value.auto_auto_provisioning_defaults != null ? [cluster_autoscaling.value.auto_auto_provisioning_defaults] : []
+        content {
+          service_account = auto_provisioning_defaults.value.service_account
+          disk_size       = auto_provisioning_defaults.value.disk_size_gb
+          disk_type       = auto_provisioning_defaults.value.disk_type
+          image_type      = auto_provisioning_defaults.value.image_type
+
+          dynamic "management" {
+            for_each = auto_provisioning_defaults.value.management != null ? [auto_provisioning_defaults.value.management] : []
+            content {
+              auto_upgrade = management.value.auto_upgrade
+              auto_repair  = management.value.auto_repair
+            }
+          }
+
+          dynamic "upgrade_settings" {
+            for_each = auto_provisioning_defaults.value.upgrade_settings != null ? [auto_provisioning_defaults.value.upgrade_settings] : []
+            content {
+              strategy        = upgrade_settings.value.strategy
+              max_surge       = upgrade_settings.value.max_surge
+              max_unavailable = upgrade_settings.value.max_unavailable
+            }
+          }
+        }
+      }
+    }
+  }
+
+  dynamic "service_external_ips_config" {
+    for_each = var.service_external_ips_config != null ? [var.service_external_ips_config] : []
+    content {
+      enabled = service_external_ips_config.value.enabled
+    }
+  }
+
+  dynamic "ip_allocation_policy" {
+    for_each = var.ip_allocation_policy != null ? [var.ip_allocation_policy] : []
+    content {
+      cluster_secondary_range_name  = ip_allocation_policy.value.cluster_secondary_range_name
+      services_secondary_range_name = ip_allocation_policy.value.services_secondary_range_name
+      cluster_ipv4_cidr_block       = ip_allocation_policy.value.cluster_ipv4_cidr_block
+      services_ipv4_cidr_block      = ip_allocation_policy.value.services_ipv4_cidr_block
+      stack_type                    = ip_allocation_policy.value.stack_type
+    }
+  }
+
+  dynamic "maintenance_policy" {
+    for_each = var.maintenance_policy != null ? [var.maintenance_policy] : []
+    content {
+
+      dynamic "daily_maintenance_window" {
+        for_each = maintenance_policy.value.daily_maintenance_window != null ? [maintenance_policy.value.daily_maintenance_window] : []
+        content {
+          start_time = daily_maintenance_window.value.start_time
+        }
+      }
+
+      dynamic "recurring_window" {
+        for_each = maintenance_policy.value.recurring_window != null ? [maintenance_policy.value.recurring_window] : []
+        content {
+          start_time = recurring_window.value.start_time
+          end_time   = recurring_window.value.end_time
+          recurrence = recurring_window.value.recurrence
+        }
+      }
+
+      dynamic "maintenance_exclusion" {
+        for_each = maintenance_policy.value.maintenance_exclusion != null ? [maintenance_policy.value.maintenance_exclusion] : []
+        content {
+          exclusion_name = maintenance_exclusion.value.exclusion_name
+          start_time     = maintenance_exclusion.value.start_time
+          end_time       = maintenance_exclusion.value.end_time
+
+          dynamic "exclusion_options" {
+            for_each = maintenance_exclusion.value.exclusion_options != null ? [maintenance_exclusion.value.exclusion_options] : []
+            content {
+              scope = exclusion_options.value.scope
+            }
+          }
+        }
+      }
+    }
+  }
+
+  dynamic "master_authorized_networks_config" {
+    for_each = var.master_authorized_networks_config != null ? [var.master_authorized_networks_config] : []
+    content {
+
+      dynamic "cidr_blocks" {
+        for_each = master_authorized_networks_config.value.cidr_blocks != null ? master_authorized_networks_config.value.cidr_blocks : []
+        content {
+          cidr_block   = cidr_blocks.value.cidr_block
+          display_name = cidr_blocks.value.display_name
+        }
+      }
+
+      gcp_public_cidrs_access_enabled = master_authorized_networks_config.value.gcp_public_cidrs_access_enabled
+    }
+  }
+
+  dynamic "network_policy" {
+    for_each = var.network_policy != null ? [var.network_policy] : []
+    content {
+      provider = network_policy.value.provider
+      enabled  = network_policy.value.enabled
+    }
+  }
+
+  dynamic "node_pool_auto_config" {
+    for_each = var.node_pool_auto_config != null ? [var.node_pool_auto_config] : []
+    content {
+
+      dynamic "node_kubelet_config" {
+        for_each = node_pool_auto_config.value.node_kubelet_config != null ? [node_pool_auto_config.value.node_kubelet_config] : []
+        content {
+          insecure_kubelet_readonly_port_enabled = node_kubelet_config.value.insecure_kubelet_readonly_port_enabled
+        }
+      }
+
+      resource_manager_tags = node_pool_auto_config.value.resource_manager_tags
+
+      dynamic "network_tags" {
+        for_each = node_pool_auto_config.value.network_tags != null ? node_pool_auto_config.value.network_tags : []
+        content {
+          tags = network_tags.value
+        }
+      }
+    }
+  }
+
+  lifecycle {
+    precondition {
+      condition     = !(var.node_locations == null && var.cluster_location == null)
+      error_message = "Either cluster_location or node_locations must be provided. Unless your cluster nodes need to be in specific zones, please provide the value for cluster_location."
+    }
+  }
+}
--- variables.tf
+++ variables.tf
@@ -1,336 +1,279 @@
-# Variable definitions
+variable "project_id" {
+  description = "The project ID where the resources will be provisioned."
+  type        = string
+}
+
 variable "cluster_name" {
-  description = "The name for the GKE cluster."
+  description = "The name of the cluster."
   type        = string
 }
 
 variable "cluster_location" {
-  description = "The location (region or zone) for the GKE cluster."
-  type        = string
-}
-
-variable "description" {
-  description = "The description for the GKE cluster."
+  description = "The location of the cluster. This can be a region or a zone."
   type        = string
   default     = null
 }
 
-variable "project_id" {
-  description = "The project ID for the GKE cluster."
-  type        = string
-}
-
 variable "node_locations" {
-  description = "A list of zones for the GKE nodes."
+  description = "The location for the nodes. When defining this value, the cluster location is omitted."
   type        = set(string)
-  default     = []
+  default     = null
 }
 
-variable "cluster_deletion_protection" {
-  description = "Whether or not to allow Terraform to destroy the GKE cluster (and all contained resources)."
+variable "enable_cluster_deletion_protection" {
+  description = "If Terraform is allowed to delete the cluster."
   type        = bool
   default     = true
 }
 
 variable "allow_net_admin" {
-  description = "Whether or not to allow the GKE nodes to have the 'net_admin' capability."
+  description = "If NET_ADMIN is enabled for the cluster."
   type        = bool
   default     = false
 }
 
-variable "cluster_ipv4_cidr" {
-  description = "The IP address range for the pods in this cluster. If left blank a /14 block will be used."
+variable "cluster_description" {
+  description = "The description of the cluster."
   type        = string
   default     = null
 }
 
 variable "enable_autopilot" {
-  description = "Whether or not to enable Autopilot for the GKE cluster."
-  type        = bool
-  default     = false
-}
-
-variable "enable_tpu" {
-  description = "Whether or not to enable TPUs for the GKE cluster."
+  description = "If autopilot is enabled for the cluster."
   type        = bool
   default     = false
 }
 
 variable "initial_node_count" {
-  description = "The initial number of nodes for the GKE cluster."
+  description = "The initial node count for the cluster."
   type        = number
-  default     = null
+  default     = 1
 }
 
-variable "remove_default_node_pool" {
-  description = "Whether or not to remove the default node pool from the GKE cluster."
-  type        = bool
-  default     = false
+variable "networking_mode" {
+  description = "The networking mode for the cluster."
+  type        = string
+  default     = "VPC_NATIVE"
+
+  validation {
+    condition     = contains(["VPC_NATIVE", "ROUTES"], var.networking_mode)
+    error_message = "networking_mode must be either VPC_NATIVE or ROUTES."
+  }
+}
+
+variable "network" {
+  description = "The network for the cluster."
+  type        = string
 }
 
 variable "addons_config" {
-  description = "The addons config for the GKE cluster."
+  description = "The addons configuration for the cluster."
   type = object({
-    disable_horizontal_pod_autoscaling = optional(object({
-      disabled = bool
-    }))
-    disable_http_load_balancing = optional(object({
-      disabled = bool
-    }))
-    disable_network_policy_config = optional(object({
-      disabled = bool
-    }))
-    enable_gcp_filestore_csi_driver_config = optional(object({
-      enabled = bool
-    }))
-    enable_gcs_fuse_csi_driver_config = optional(object({
-      enabled = bool
-    }))
+    disable_horizontal_pod_autoscaling = bool
+    disable_http_load_balancing        = bool
+    disable_network_policy_config      = bool
+    enable_gcp_filestore_csi_driver    = bool
+    enable_gcs_fuse_csi_driver         = bool
     cloudrun_config = optional(object({
-      disabled           = bool
-      load_balancer_type = string
-    }))
-    dns_cache_config = optional(object({
-      enabled = bool
-    }))
-    gce_persistent_disk_csi_driver_config = optional(object({
-      enabled = bool
-    }))
-    gke_backup_agent_config = optional(object({
-      enabled = bool
+      disable_cloudrun_config = bool
+      load_balancer_type      = string
     }))
   })
-  default = null
+  default = {
+    disable_horizontal_pod_autoscaling = false
+    disable_http_load_balancing        = false
+    disable_network_policy_config      = false
+    enable_gcp_filestore_csi_driver    = false
+    enable_gcs_fuse_csi_driver         = false
+    cloudrun_config                    = null
+  }
+
+  validation {
+    condition     = var.addons_config.cloudrun_config == null ? true : contains(["LOAD_BALANCER_TYPE_INTERNAL", "LOAD_BALANCER_TYPE_EXTERNAL"], var.addons_config.cloudrun_config.load_balancer_type)
+    error_message = "The load_balancer_type must be either LOAD_BALANCER_TYPE_INTERNAL or LOAD_BALANCER_TYPE_EXTERNAL."
+  }
 }
 
 variable "cluster_autoscaling" {
-  description = "The cluster autoscaling config for the GKE cluster."
+  description = "The cluster autoscaling configuration for the cluster."
   type = object({
-    enabled = optional(bool)
+    enable_cluster_autoscaling = bool
     resource_limits = optional(set(object({
       resource_type = string
       minimum       = number
       maximum       = number
     })))
     auto_auto_provisioning_defaults = optional(object({
-      min_cpu_platform = string
-      service_account  = string
-      disk_size        = string
-      disk_type        = string
-      image_type       = string
+      service_account = optional(string)
+      disk_size_gb    = optional(number)
+      disk_type       = optional(string)
+      image_type      = optional(string)
       management = optional(object({
         auto_upgrade = bool
         auto_repair  = bool
-        upgrade_settings = optional(object({
-          strategy        = string
-          max_surge       = number
-          max_unavailable = number
-        }))
+      }))
+      upgrade_settings = optional(object({
+        strategy        = string
+        max_surge       = number
+        max_unavailable = number
       }))
     }))
   })
-  default = null
+  default = {
+    enable_cluster_autoscaling = false
+    resource_limits            = null
+    auto_auto_provisioning_defaults = {
+      management = null
+      upgrade_settings = {
+        strategy        = "SURGE"
+        max_surge       = 1
+        max_unavailable = 0
+      }
+    }
+  }
+
+  validation {
+    condition     = var.cluster_autoscaling.auto_auto_provisioning_defaults.service_account == null ? true : can(regex("^[a-z]([-a-z0-9]*[a-z0-9])?$", var.cluster_autoscaling.auto_auto_provisioning_defaults.service_account))
+    error_message = "service_account must be a valid service account name"
+  }
+
+  validation {
+    condition = var.cluster_autoscaling.resource_limits == null ? true : alltrue([
+      for limit in var.cluster_autoscaling.resource_limits :
+      contains(["cpu", "memory"], lower(limit.resource_type))
+    ])
+    error_message = "resource_type must be either 'cpu' or 'memory'"
+  }
 }
 
-variable "service_external_ip_configs" {
-  description = "The service external ip configs for the GKE cluster."
+variable "service_external_ips_config" {
+  description = "The service external IPs configuration for the cluster."
   type = object({
     enabled = bool
   })
-  default = null
-}
-
-variable "default_max_pods_per_node" {
-  description = "The default max pods per node for the GKE cluster."
-  type        = number
-  default     = null
+  default = {
+    enabled = false
+  }
 }
 
 variable "ip_allocation_policy" {
-  description = "The configuration for the cluster pod and services network ranges."
+  description = "The IP allocation policy configuration for the cluster."
   type = object({
-    cluster_secondary_range_name  = optional(string)
-    services_secondary_range_name = optional(string)
-    cluster_ipv4_cidr_block       = optional(string)
-    services_ipv4_cidr_block      = optional(string)
-    stack_type                    = optional(string)
+    cluster_secondary_range_name  = string
+    services_secondary_range_name = string
+    cluster_ipv4_cidr_block       = string
+    services_ipv4_cidr_block      = string
+    stack_type                    = string
   })
-  default = null
-}
-
-variable "networking_mode" {
-  description = "If the cluster network is ROUTES or VPC_NATIVE. This affects the alias IPs and routes."
-  type        = string
-  default     = "VPC_NATIVE"
 
   validation {
-    condition     = contains(["ROUTES", "VPC_NATIVE"], var.networking_mode)
-    error_message = "networking_mode must be either ROUTES or VPC_NATIVE."
+    condition     = can(regex("^([0-9]{1,3}\\.){3}[0-9]{1,3}/([0-9]|[1-2][0-9]|3[0-2])$", var.ip_allocation_policy.cluster_ipv4_cidr_block))
+    error_message = "cluster_ipv4_cidr_block must be a valid IPv4 CIDR notation"
+  }
+
+  validation {
+    condition     = can(regex("^([0-9]{1,3}\\.){3}[0-9]{1,3}/([0-9]|[1-2][0-9]|3[0-2])$", var.ip_allocation_policy.services_ipv4_cidr_block))
+    error_message = "services_ipv4_cidr_block must be a valid IPv4 CIDR notation"
+  }
+
+  validation {
+    condition     = contains(["IPV4", "IPV4_IPV6"], var.ip_allocation_policy.stack_type)
+    error_message = "stack_type must be either IPV4 or IPV4_IPV6"
   }
 }
 
 variable "maintenance_policy" {
-  description = "Maintenance configuration for the GKE cluster"
+  description = "The maintenance policy configuration for the cluster."
   type = object({
     daily_maintenance_window = optional(object({
-      start_time = optional(string) # Format: HH:MM, in UTC
+      start_time = string
     }))
     recurring_window = optional(object({
-      start_time = optional(string) # RFC3339 timestamp
-      end_time   = optional(string) # RFC3339 timestamp
-      recurrence = optional(string) # RFC5545 RRULE
+      start_time = string
+      end_time   = string
+      recurrence = string
     }))
-    maintenance_exclusion = optional(object({
-      exclusion_name = optional(string)
+    maintenance_exclusion = optional(set(object({
+      exclusion_name = string
+      start_time     = string
+      end_time       = string
       exclusion_options = optional(object({
-        scope = optional(string) # NO_UPGRADES, NO_MINOR_UPGRADES, or NO_MINOR_OR_NODE_UPGRADES
+        scope = string
       }))
-      start_time = optional(string) # RFC3339 timestamp
-      end_time   = optional(string) # RFC3339 timestamp
-    }))
+    })))
   })
-
-  default = null
+  default = {
+    daily_maintenance_window = null
+    recurring_window         = null
+    maintenance_exclusion    = null
+  }
 
   validation {
-    condition     = var.maintenance_policy == null || try(can(regex("^([0-1][0-9]|2[0-3]):[0-5][0-9]$", var.maintenance_policy.daily_maintenance_window.start_time)), true)
-    error_message = "daily_maintenance_window start_time must be in HH:MM format (00:00-23:59)."
+    condition     = var.maintenance_policy.daily_maintenance_window == null ? true : can(regex("^([0-1][0-9]|2[0-3]):[0-5][0-9]$", var.maintenance_policy.daily_maintenance_window.start_time))
+    error_message = "daily_maintenance_window start_time must be in HH:mm format (00:00-23:59)"
+  }
+
+  validation {
+    condition = var.maintenance_policy.recurring_window == null ? true : (
+      can(regex("^[0-9]{4}-[0-9]{2}-[0-9]{2}T([0-1][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]Z$", var.maintenance_policy.recurring_window.start_time)) &&
+    can(regex("^[0-9]{4}-[0-9]{2}-[0-9]{2}T([0-1][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]Z$", var.maintenance_policy.recurring_window.end_time)))
+    error_message = "recurring_window times must be in RFC3339 format"
+  }
+
+  validation {
+    condition     = var.maintenance_policy.recurring_window == null ? true : can(regex("^FREQ=(DAILY|WEEKLY|MONTHLY);(BYDAY=[A-Z,]+)?$", var.maintenance_policy.recurring_window.recurrence))
+    error_message = "recurring_window recurrence must be in iCal format (e.g., FREQ=WEEKLY;BYDAY=MO,TU,WE,TH,FR)"
   }
 }
 
-variable "network" {
-  description = "The VPC where the cluster is connected."
-  type        = string
+variable "master_authorized_networks_config" {
+  description = "The master authorized networks configuration for the cluster."
+  type = object({
+    cidr_blocks = optional(set(object({
+      cidr_block   = string
+      display_name = string
+    })))
+    gcp_public_cidrs_access_enabled = bool
+  })
+  default = {
+    cidr_blocks                     = null
+    gcp_public_cidrs_access_enabled = false
+  }
+
+  validation {
+    condition = var.master_authorized_networks_config.cidr_blocks == null ? true : alltrue([
+      for block in var.master_authorized_networks_config.cidr_blocks : can(regex("^([0-9]{1,3}\\.){3}[0-9]{1,3}/([0-9]|[1-2][0-9]|3[0-2])$", block.cidr_block))
+    ])
+    error_message = "cidr_blocks must be valid IPv4 CIDR notation"
+  }
 }
 
-variable "subnetwork" {
-  description = "The VPC subnetwork where the cluster is connected."
-  type        = string
+variable "network_policy" {
+  description = "The network policy configuration for the cluster."
+  type = object({
+    provider = string
+    enabled  = bool
+  })
+  default = {
+    provider = "PROVIDER_UNSPECIFIED"
+    enabled  = false
+  }
 }
 
 variable "node_pool_auto_config" {
-  description = "Default values for node pools managed by autopilot."
+  description = "The node pool auto configuration for the cluster."
   type = object({
-    insecure_kubelet_readonly_port_enabled = optional(bool)
-    resource_manager_tags = optional(object({
-      "name" = string
+    node_kubelet_config = optional(object({
+      insecure_kubelet_readonly_port_enabled = bool
     }))
-    network_tags = optional(set(string))
-  })
-}
-
-variable "node_pool_defaults" {
-  description = "Default values for node pools managed by autopilot."
-  type = object({
-    node_config_defaults = optional(object({
-      insecure_kubelet_readonly_port_enabled = optional(bool)
-      logging_variant                        = optional(string)
-      gcfs_config_enabled                    = optional(bool)
-    }))
+    resource_manager_tags = optional(map(string))
+    network_tags          = optional(set(string))
   })
   default = {
-    node_config_defaults = {
+    node_kubelet_config = {
       insecure_kubelet_readonly_port_enabled = false
-      logging_variant                        = "DEFAULT"
-      gcfs_config_enabled                    = null
     }
-  }
-}
-
-variable "release_channel" {
-  description = "The release channel for Kubernetes."
-  type        = string
-  default     = "UNSPECIFIED"
-
-  validation {
-    condition     = contains(["UNSPECIFIED", "RAPID", "REGULAR", "STABLE", "EXTENDED"], var.release_channel)
-    error_message = "release_channel must be one of UNSPECIFIED, RAPID, REGULAR, STABLE, or EXTENDED."
-  }
-}
-
-variable "resource_labels" {
-  description = "Labels to apply to the cluster."
-  type        = map(string)
-  default     = null
-}
-
-variable "enable_vertical_pod_autoscaling" {
-  description = "Whether or not to enable vertical pod autoscaling for the GKE cluster."
-  type        = bool
-  default     = false
-}
-
-variable "workload_identity_pool" {
-  description = "The workload identity pool for the GKE cluster."
-  type        = string
-  default     = null
-}
-
-variable "dns_config" {
-  description = "Cluster DNS settings."
-  type = object({
-    cluster_dns       = optional(string)
-    cluster_dns_scope = optional(string)
-  })
-  default = {
-    cluster_dns       = "PROVIDER_UNSPECIFIED"
-    cluster_dns_scope = "DNS_SCOPE_UNSPECIFIED"
-  }
-
-  validation {
-    condition     = contains(["PROVIDER_UNSPECIFIED", "PLATFORM_DEFAULT", "CLOUD_DNS"], var.dns_config.cluster_dns)
-    error_message = "release_channel must be one of PROVIDER_UNSPECIFIED, PLATFORM_DEFAULT, CLOUD_DNS."
-  }
-
-  validation {
-    condition     = contains(["DNS_SCOPE_UNSPECIFIED", "CLUSTER_SCOPE", "VPC_SCOPE"], var.dns_config.cluster_dns_scope)
-    error_message = "release_channel must be one of DNS_SCOPE_UNSPECIFIED, CLUSTER_SCOPE, VPC_SCOPE."
-  }
-}
-
-# Node pool configuration
-variable "node_pools" {
-  description = "List of node pool configurations"
-  type = list(object({
-    name                      = string
-    machine_type              = string
-    node_locations            = optional(list(string))
-    autoscaling_min_count     = number
-    autoscaling_max_count     = number
-    max_pods_per_node         = optional(number)
-    node_locations            = optional(set(string))
-    location_policy           = optional(string)
-    local_ssd_count           = optional(number)
-    disk_size_gb              = optional(number)
-    disk_type                 = optional(string)
-    image_type                = optional(string)
-    auto_repair               = optional(bool)
-    auto_upgrade              = optional(bool)
-    service_account           = optional(string)
-    preemptible               = optional(bool)
-    spot                      = optional(bool)
-    initial_node_count        = optional(number)
-    management_auto_repair    = optional(bool)
-    management_auto_upgrade   = optional(bool)
-    gcfs_config_enabled       = optional(bool)
-    gvnic_enabled             = optional(bool)
-    use_preemptible_instances = optional(bool)
-    use_spot_instances        = optional(bool)
-    taint = optional(object({
-      key    = string
-      value  = string
-      effect = string
-    }))
-  }))
-  default = []
-
-  validation {
-    condition     = contains(["BALANCED", "ANY"], var.node_pools.location_policy)
-    error_message = "node_pools_location_policy must be one of BALANCED or ANY."
-  }
-
-  validation {
-    condition     = contains(["NO_SCHEDULE", "PREFER_NO_SCHEDULE", "NO_EXECUTE"], var.node_pools.taint.effect)
-    error_message = "value must be one of NO_SCHEDULE, PREFER_NO_SCHEDULE, or NO_EXECUTE."
+    resource_manager_tags = null
+    network_tags          = null
   }
 }
