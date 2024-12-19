db5a9422be96c776c79d120f77424ec1bf0a8212
Francisco Ferrinho <ferrinho@gmail.com>
2024-12-19 17:11:16 +0000

    update example, outputs and readme

---
+++ examples/simple_autopilot.tf
@@ -0,0 +1,24 @@
+data "google_compute_subnetwork" "compute" {
+  self_link = module.vpc.subnetwork_self_links[0]
+}
+
+module "gke" {
+  source = "git::git@github.com:FFerrinho/terraform-google-gke.git?ref=1.0"
+
+  cluster_name       = "cluster-fferrinho-autopilot"
+  cluster_location   = var.region
+  project_id         = "celfocus-gcp-ccoe-cfmsem-9923"
+  enable_autopilot   = true
+  network            = module.vpc.vpc_name
+  cluster_subnetwork = data.google_compute_subnetwork.compute.self_link
+  kubernetes_release_channel = "STABLE"
+
+  ip_allocation_policy = {
+    cluster_ipv4_cidr_block       = data.google_compute_subnetwork.compute.secondary_ip_range[0].ip_cidr_range
+    cluster_secondary_range_name  = data.google_compute_subnetwork.compute.secondary_ip_range[0].range_name
+    services_ipv4_cidr_block      = data.google_compute_subnetwork.compute.secondary_ip_range[1].ip_cidr_range
+    services_secondary_range_name = data.google_compute_subnetwork.compute.secondary_ip_range[1].range_name
+    stack_type                    = "IPV4"
+  }
+}
+
--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,43 @@ output "deprecation_warnings" {
     preemptible = "⚠️ Warning: Preemptible VMs are being deprecated by Google. Please consider using Spot VMs instead by setting spot_enabled = true"
   } : null
 }
+
+output "cluster_name" {
+  description = "The name of the Kubernetes cluster"
+  value       = google_container_cluster.main.name
+}
+
+output "cluster_endpoint" {
+  description = "The endpoint of the Kubernetes cluster"
+  value       = google_container_cluster.main.endpoint
+}
+
+output "cluster_ca_certificate" {
+  description = "The CA certificate of the Kubernetes cluster"
+  value       = base64decode(google_container_cluster.main.master_auth.0.cluster_ca_certificate)
+}
+
+output "client_certificate" {
+  description = "The client certificate used to authenticate to the Kubernetes cluster"
+  value       = google_container_cluster.main.master_auth.0.client_certificate
+}
+
+output "client_key" {
+  description = "The client key used to authenticate to the Kubernetes cluster"
+  value       = google_container_cluster.main.master_auth.0.client_key
+}
+
+output "node_pools" {
+  description = "The list of node pools in the Kubernetes cluster"
+  value       = google_container_cluster.main.node_pool
+}
+
+output "cluster_ipv4_cidr" {
+  description = "The IP address range of the Kubernetes pods"
+  value       = google_container_cluster.main.cluster_ipv4_cidr
+}
+
+output "services_ipv4_cidr" {
+  description = "The IP address range of the Kubernetes services"
+  value       = google_container_cluster.main.services_ipv4_cidr
+}
