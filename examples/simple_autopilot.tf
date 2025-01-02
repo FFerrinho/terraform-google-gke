80ba704f4e5d8cde4d1d80df5510ddfde97d2941
Francisco Ferrinho <52923602+FFerrinho@users.noreply.github.com>
2025-01-02 10:35:01 +0000

    Update simple_autopilot.tf

--- examples/simple_autopilot.tf
+++ examples/simple_autopilot.tf
@@ -5,12 +5,12 @@ data "google_compute_subnetwork" "compute" {
 module "gke" {
   source = "git::git@github.com:FFerrinho/terraform-google-gke.git?ref=1.0"
 
-  cluster_name       = "cluster-fferrinho-autopilot"
-  cluster_location   = var.region
-  project_id         = "celfocus-gcp-ccoe-cfmsem-9923"
-  enable_autopilot   = true
-  network            = module.vpc.vpc_name
-  cluster_subnetwork = data.google_compute_subnetwork.compute.self_link
+  cluster_name               = "cluster-fferrinho-autopilot"
+  cluster_location           = var.region
+  project_id                 = "celfocus-gcp-ccoe-cfmsem-9923"
+  enable_autopilot           = true
+  network                    = module.vpc.vpc_name
+  cluster_subnetwork         = data.google_compute_subnetwork.compute.self_link
   kubernetes_release_channel = "STABLE"
 
   ip_allocation_policy = {
@@ -21,4 +21,3 @@ module "gke" {
     stack_type                    = "IPV4"
   }
 }
-
